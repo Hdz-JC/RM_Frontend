ea0b7fcb38374b307e7f410cc349c27bcb3b4812
hdz_jc <a22150084@itmexicali.edu.mx>
2026-08-20 14:31:17 -0700

    New Link

--- src/keycloak.js
+++ src/keycloak.js
@@ -1,7 +1,7 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-    url: "http://localhost:8180",
+    url: "https://453jp20r-8180.usw3.devtunnels.ms/",
     realm: "Routines-Manager",
     clientId: "Routines-Test"
 });
