b20ce9bb0b4bc5a4506ed5fa3affc58aa97ea9b5
hdz_jc <a22150084@itmexicali.edu.mx>
2026-08-21 05:57:09 -0700

    Final Test

--- src/services/api.js
+++ src/services/api.js
@@ -1,20 +1,12 @@
 import axios from "axios";
 import keycloak from "../keycloak";
 
-console.log("🔥 API.JS CARGADO");
-console.log("🔥 API BASE:", "https://453jp20r-3000.devtunnels.ms/api");
-
 const api = axios.create({
     baseURL: "https://453jp20r-3000.devtunnels.ms/api"
 });
 
 api.interceptors.request.use(
     async (config) => {
-
-        console.log("🔥 INTERCEPTOR EJECUTADO");
-        console.log("🔥 URL:", config.url);
-        console.log("🔥 AUTH:", keycloak.authenticated);
-        console.log("🔥 TOKEN:", keycloak.token ? "SI" : "NO");
 
         if (keycloak.authenticated) {
             await keycloak.updateToken(30);
